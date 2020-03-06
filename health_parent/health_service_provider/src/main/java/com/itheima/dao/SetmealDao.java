f583867a397b6620770eb817d97ae7ec651767fe
79412906@qq.com <79412906zhang>
2020-03-06 10:24:47 +0800

    添加Mobile 和模版化

--- health_parent/health_service_provider/src/main/java/com/itheima/dao/SetmealDao.java
+++ health_parent/health_service_provider/src/main/java/com/itheima/dao/SetmealDao.java
@@ -25,4 +25,6 @@ public interface SetmealDao {
     Setmeal finSetmealById(Integer id);
 
     List<String> findAllsetmealNames();
+
+    Object findById(Integer id);
 }
--- health_parent/health_service_provider/src/main/java/com/itheima/service/impl/SetmealServiceImpl.java
+++ health_parent/health_service_provider/src/main/java/com/itheima/service/impl/SetmealServiceImpl.java
@@ -14,10 +14,18 @@ import com.itheima.pojo.Setmeal;
 import com.itheima.service.SetmealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import redis.clients.jedis.JedisPool;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 检查项服务
@@ -35,6 +43,11 @@ public class SetmealServiceImpl implements SetmealService{
 
     @Autowired
     private JedisPool jedisPool;
+
+    @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+    private String outPutPath ="F:/IdeaProjects/healthCZ/health_mobile/src/main/webapp/pages/";//从属性文件中读取要生成的html对应的目录
+
 
     public void setJedisPool(JedisPool jedisPool) {
         this.jedisPool = jedisPool;
@@ -77,7 +90,51 @@ public class SetmealServiceImpl implements SetmealService{
         List<Setmeal> list = setmealDao.findAllSetmeal();
         return list;
     }
+    //生成静态页面
+    public void generateMobileStaticHtml(){
+        List<Setmeal> list = setmealDao.findAllSetmeal();
+        getSerMealDetailed(list);
+        getSetMeal(list);
+    }
 
+    //生成套餐详情界面
+    public void getSerMealDetailed(List<Setmeal> list){
+        Map map = new HashMap();
+        for (Setmeal setmeal : list) {
+            map.put("setmeal",setmealDao.finSetmealById(setmeal.getId()));
+            getHtml("mobile_setmeal_detail.ftl","setmeal_detail_" + setmeal.getId() + ".html",map);
+        }
+    }
+    //套餐列表静态界面
+    public void getSetMeal(List<Setmeal> list){
+        Map map = new HashMap();
+        map.put("setmealList",list);
+        getHtml("mobile_setmeal.ftl","m_setmeal.html",map);
+    }
+    //生成静态界面
+    public void getHtml(String ftlName,String htmlName,Map map){
+        Configuration configuration = freeMarkerConfigurer.getConfiguration();
+        Writer writer = null;
+        try {
+            //加载模板文件
+            Template template = configuration.getTemplate(ftlName);
+            writer = new FileWriter(outPutPath+htmlName);
+            //输出
+            template.process(map,writer);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(writer != null){
+                //关闭流
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
     @Override
     public Setmeal findSetmealById(int id) {
         Setmeal setmeal = setmealDao.finSetmealById(id);
